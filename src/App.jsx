81bbcbe9bcb6ea4943661e36d8c1a25cbd4eb6d5
Heshani833 <heshanishehana123@gmail.com>
2025-04-26 14:09:41 +0530

    Complete Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,66 @@
 import { useState } from "react";
-
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState({
+    fullname: "",
+    email: "",
+    role: "",
+  });
+
+  const handleChange = (e) => {
+    setValue({ ...value, [e.target.name]: [e.target.value] });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(value);
+  };
+
   return (
     <>
       <div className="container">
-        <h1>Registration Form</h1>
-        <form>
+        <h1>Ueser Information Form</h1>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="fullname">Full Name</label>
-          <input type="text" placeholder="Enter Full Name" name="fullname" />
+          <input
+            type="text"
+            placeholder="Enter your full name"
+            name="fullname"
+            onChange={(e) => handleChange(e)}
+            required
+          />
           <label htmlFor="email">Email</label>
-          <input type="text" placeholder="Enter Email" name="email" />
+          <input
+            type="text"
+            placeholder="Enter your email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+            required
+          />
           <label htmlFor="role">Select Your Role</label>
-          <input type="radio" name="role" />
-          Student
-          <input type="radio" name="role" />
-          Employee
+          <div className="radio-group">
+            <div>
+              <input
+                type="radio"
+                id="student"
+                name="role"
+                value="student"
+                onChange={(e) => handleChange(e)}
+              />
+              <label htmlFor="student">Student</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="employee"
+                name="role"
+                value="employee"
+                onChange={(e) => handleChange(e)}
+              />
+              <label htmlFor="employee">Employee</label>
+            </div>
+          </div>
           <button>Submit</button>
         </form>
       </div>
