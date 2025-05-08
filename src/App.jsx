7fdb49ead6753fc63a6844c4c0c01fa5c07e9ebf
Heshani833 <heshanishehana123@gmail.com>
2025-05-08 15:35:59 +0530

    adding background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
   const [value, setValue] = useState({
     fullname: "",
     email: "",
@@ -117,6 +117,6 @@ function App() {
       </form>
     </div>
   );
-}
+};
 
 export default App;
